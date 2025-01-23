8e95d12bfc78f17cd043e9fa045550d9f0653ed4
Samuel11-cell <samuel11narvaez@gmail.com>
2025-01-22 19:29:11 -0500

    Update impute1.R

--- impute1.R
+++ impute1.R
@@ -1,1 +1,3 @@
-# Include here your code for your first chosen imputation method
+# First chosen imputation method: IMputing the missing values with the median
+db <- db  %>%
+  mutate(ingtot = ifelse(is.na(ingtot) == TRUE, median(db$ingtot, na.rm = TRUE) , ingtot))
