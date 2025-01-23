45d36bfcb656f9fa49751a3e64162e4e937be33b
Laura-BN <lbuitrago41@gmail.com>
2025-01-22 19:29:28 -0500

    Update impute2.R

--- impute2.R
+++ impute2.R
@@ -7,6 +7,8 @@ dummy_maxEducLevel <- as.data.frame(model.matrix(~ maxEducLevel - 1, data = db))
 #db<- db  %>% cbind(dummy_maxEducLevel) 
 db <- cbind(db, dummy_maxEducLevel)
 
+db %>% select(maxEducLevel,maxEducLevel1, maxEducLevel2,maxEducLevel3 ,maxEducLevel4 ,maxEducLevel5 ,maxEducLevel6, maxEducLevel7 ) %>% head() 
+
 linear_imput_model <- lm(y_salary_m ~ ingtot + sex   + maxEducLevel3 + maxEducLevel4 + maxEducLevel5 + maxEducLevel6 + maxEducLevel7 , data = db)
 summary(linear_imput_model)
 
